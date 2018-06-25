685e8e7bdc316606d552f8d9846f97ed3294acf9
chaiyaph0n <ball.chaiyaphon@gmail.com>
2018-06-25 22:41:57 +0700

    First commit - Project setup

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const webpack = require("webpack");
+
+module.exports = {
+  entry: ["react-hot-loader/patch", "./src/index.js"],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"]
+  },
+  output: {
+    path: __dirname + "/dist",
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    contentBase: "./dist",
+    host: "127.0.0.1",
+    port: 3000,
+    hot: true
+  },
+  devtool: "eval-source-map"
+};
